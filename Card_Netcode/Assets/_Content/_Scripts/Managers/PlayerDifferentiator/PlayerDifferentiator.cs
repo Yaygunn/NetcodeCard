cdf812c1623f11ec4f2bba0d84716652509ae485
Yaygun <yusufaygunnn@hotmail.com>
2024-10-21 22:43:50 +0300

    card play scene

--- Card_Netcode/Assets/_Content/_Scripts/Managers/PlayerDifferentiator/PlayerDifferentiator.cs
+++ Card_Netcode/Assets/_Content/_Scripts/Managers/PlayerDifferentiator/PlayerDifferentiator.cs
@@ -8,9 +8,19 @@ namespace Yaygun
 		[Header("CamPos")]
 		[SerializeField] private Transform[] _camTransforms;
 
+        [Header("Hands")]
+        [SerializeField] private Transform _handParent;
+
         protected override void Initialize()
         {
+            SetHandPos();
             SetCamPos();
+        }
+
+        private void SetHandPos()
+        {
+            if (PlayerData.Instance.PlayerIndex == 1)
+                _handParent.Rotate(0, 0, 180);
         }
 
         private void SetCamPos()
