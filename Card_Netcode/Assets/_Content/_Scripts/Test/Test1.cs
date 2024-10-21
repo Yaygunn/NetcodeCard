f174010ddc7b1aee02a926a28c943e3ee19e553f
Yaygun <yusufaygunnn@hotmail.com>
2024-10-21 18:55:47 +0300

    remove previous model & movement

--- Card_Netcode/Assets/_Content/_Scripts/Test/Test1.cs
+++ Card_Netcode/Assets/_Content/_Scripts/Test/Test1.cs
@@ -5,19 +5,12 @@ namespace Yaygun.Test
 {
 	public class Test1 : NetworkBehaviour
 	{
-        [SerializeField] private GameObject _model;
-        private NetworkVariable<Vector2> _networkPosition = new NetworkVariable<Vector2>(Vector2.zero, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
                 GetPlayerIndexServerRpc();
             }
-
-            _networkPosition.OnValueChanged += SetPos;
-            _model = Instantiate(_model);
-            
         }
 
         private void Update()
@@ -25,17 +18,9 @@ namespace Yaygun.Test
             if (!IsOwner)
                 return;
 
-            float verticalInput = Input.GetAxis("Vertical");
-            float horizontalInput = Input.GetAxis("Horizontal");
-
-            Vector2 movement = new Vector2 (horizontalInput, verticalInput) * 3 * Time.deltaTime;
-
-            _networkPosition.Value = (Vector2)_model.transform.position + movement;
-
             if (Input.GetKeyDown(KeyCode.C))
             {
                 PerformActionServerRpc();
-                //GetPlayerIndexServerRpc();
             }
         }
 
@@ -45,10 +30,6 @@ namespace Yaygun.Test
             GameManager.Instance.IncreaseScoreServerRpc();
             Debug.Log($"{(int)OwnerClientId} performed an action on the server.");
             
-        }
-        private void SetPos(Vector2 previousValue, Vector2 newValue)
-        {
-            _model.transform.position = newValue;
         }
 
         [ServerRpc]
